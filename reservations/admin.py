a35610578aa21fb74b0347070a22cdc311f02f50
Jedrzej44 <poczter7@gmail.com>
2024-06-06 18:46:11 +0200

    updated models and admin

--- reservations/admin.py
+++ reservations/admin.py
@@ -4,5 +4,11 @@ from django.contrib import admin
 
 from reservations.models import Car
 
-admin.site.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    fields = ["car_types", "model", "horsepower", "seats", "price_per_day", "color"]
+    list_display = ["car_types", "model", "horsepower", "seats", "price_per_day", "color"]
+
+
+admin.site.register(Car, CarAdmin)
+
 
--- reservations/models.py
+++ reservations/models.py
@@ -4,9 +4,17 @@ class Car(models.Model):
 
     CAR_COLORS_CHOICES = [("BLACK", "black"), ("WHITE", "white"), ("RED", "red"), ("GRAPHITE", "graphite")]
     CAR_TYPES = [("SUV", "SUV"), ("SPORT", "sport"), ("STANDARD", "standard")]
-    color = models.CharField(choices=CAR_COLORS_CHOICES, max_length=16)
     car_types = models.CharField(choices=CAR_TYPES, max_length=16)
     model = models.CharField(max_length=100)
+    horsepower = models.DecimalField(decimal_places=0, max_digits=3, null=True)
     seats = models.PositiveSmallIntegerField()
     price_per_day = models.DecimalField(decimal_places=2, max_digits=6)
-    # horsepower =
+    color = models.CharField(choices=CAR_COLORS_CHOICES, max_length=16)
+
+
+class Klient(models.Model):
+
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=16)
+    birth_day = models.DateField()
+
