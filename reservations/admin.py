0a63baac29b3f15b760427d45fe432d622a663ab
Jedrzej44 <poczter7@gmail.com>
2024-06-07 22:33:13 +0200

    database ready

--- reservations/admin.py
+++ reservations/admin.py
@@ -2,13 +2,23 @@
 
 from django.contrib import admin
 
-from reservations.models import Car
+from reservations.models import Car, Client, Reservation
 
 class CarAdmin(admin.ModelAdmin):
-    fields = ["car_types", "model", "horsepower", "seats", "price_per_day", "color"]
-    list_display = ["car_types", "model", "horsepower", "seats", "price_per_day", "color"]
+    fields = ["id", "car_type", "model", "horsepower", "seats", "price_per_day", "color"]
+    list_display = ["id", "car_type", "model", "horsepower", "seats", "price_per_day", "color"]
+    readonly_fields = ["id"]
 
+class ClientAdmin(admin.ModelAdmin):
+    fields = ["email", "phone", "birth_day"]
+    list_display = ["email", "phone", "birth_day"]
+
+class ReservationAdmin(admin.ModelAdmin):
+    fields = ["id", "date_of_creation", "client", "car", "reservation_days", "total_price"]
+    list_display = ["id", "date_of_creation", "client", "car", "reservation_days", "total_price"]
+    readonly_fields = ["date_of_creation", "total_price", "id"]
 
 admin.site.register(Car, CarAdmin)
-
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Reservation, ReservationAdmin)
 
--- reservations/models.py
+++ reservations/models.py
@@ -4,17 +4,36 @@ class Car(models.Model):
 
     CAR_COLORS_CHOICES = [("BLACK", "black"), ("WHITE", "white"), ("RED", "red"), ("GRAPHITE", "graphite")]
     CAR_TYPES = [("SUV", "SUV"), ("SPORT", "sport"), ("STANDARD", "standard")]
-    car_types = models.CharField(choices=CAR_TYPES, max_length=16)
+    car_type = models.CharField(choices=CAR_TYPES, max_length=16)
     model = models.CharField(max_length=100)
     horsepower = models.DecimalField(decimal_places=0, max_digits=3, null=True)
     seats = models.PositiveSmallIntegerField()
     price_per_day = models.DecimalField(decimal_places=2, max_digits=6)
     color = models.CharField(choices=CAR_COLORS_CHOICES, max_length=16)
 
+    def __str__(self):
+        return f"{self.id}/{self.car_type}/{self.model}/{self.color}/{self.price_per_day}"
 
-class Klient(models.Model):
+    #dlaczego to jest w rezerwacji??
+
+
+class Client(models.Model):
 
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=16)
     birth_day = models.DateField()
 
+    def __str__(self):
+        return f"{self.id}/{self.email}"
+
+class Reservation(models.Model):
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.PROTECT)
+    reservation_days = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10, editable=False)
+
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.car.price_per_day * self.reservation_days
+        super(Reservation, self).save(*args, **kwargs)
